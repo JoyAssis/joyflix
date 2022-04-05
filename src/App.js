de79abfeefd3141a44ed828edb4aadea1c955f14
Joy <joyceassis96@gmail.com>
2022-04-04 22:15:12 -0300

    Etilizando com styed-component

--- src/App.js
+++ src/App.js
@@ -2,28 +2,69 @@ import React, { Component } from "react";
 import Home from "./components/Home";
 import Movies from "./components/Movies"
 import Series from "./components/Series"
-//
+import styled from "styled-components"
+import {createGlobalStyle} from "styled-components"
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+
+const GlobalStyle = createGlobalStyle`
+*{
+  margin:0;
+  padding:0;
+  box-sizing:border-box;
+  list-style:none;
+  a:hover {
+  color: hotpink; }
+  a:link {
+    text-decoration: none; }
+  }
+  `
+  const Title = styled.h1`
+  color: #f28482;
+  margin: 10px; 
+  text-decoration: underline; 
+  `
+  const Menu = styled.nav`
+  padding: 1vw;
+  height: 2vh;
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  `
+  const List = styled.ul`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  pading: 1vw;
+  font-size: 2vw;
+  `
+  const Item = styled.li`
+  border-bottom:3px solid white;
+  padding:0.5vw;
+  width: 100%;
+  font-size: 2vw;
+  `
 
 class App extends Component {
   render () {
     return (
     <div>
-      <h1>JOYFLIX</h1>
+      <GlobalStyle/>
+      <Title>JOYFLIX</Title>
       <Router>        
-        <nav>
-          <ul>
-            <li>
+        <Menu>
+          <List>
+            <Item>
               <Link to="/">Home</Link>
-            </li>
-            <li>
+            </Item>
+            <Item>
               <Link to="/movies">Movies</Link>
-            </li>
-            <li>
+            </Item>
+            <Item>
               <Link to="/series">Series</Link>
-            </li>
-          </ul>
-        </nav>
+            </Item>
+          </List>
+        </Menu>
 
         <Routes>
           <Route path="/" element={<Home/>}/>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+//import img from "./src/img/claquete.png"
+
+
+const Container = styled.div`
+background-color: #f7ede2;
+height: 76vh;
+display: flex;
+justify-content: flex-start;
+`
+const Box = styled.section`
+height: 55vh;
+width: 50vw;
+margin: 5vw;
+border: 2px solid black;
+border-radius: 170px;
+`
 
 function Home() {
     return (
-        <div>
-            <h2>Aqui é a Home!</h2>
-        </div>
+        <Container>
+            
+            <Box>
+            <h2>imagem aqui</h2>
+            </Box> 
+        </Container>
     );
 }
 
